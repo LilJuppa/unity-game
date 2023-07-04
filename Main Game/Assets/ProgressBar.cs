92f82e0d5e0f1aacf713dc9de3f8db09706d290b
LilJuppa <111934032+LilJuppa@users.noreply.github.com>
2023-07-03 22:19:54 -0700

    added bar

---
+++ Main Game/Assets/ProgressBar.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[ExecuteInEditMode()]
+public class ProgressBar : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [MenuItem("GameObject/UI/Linear Progress Bar")]
+    public static void AddLinearProgressBar()
+    {
+        GameObject obj = Instantiate(Resources.Load<GameObject>("UI/Linear Progress Bar"));
+        obj.transform.SetParent(Selection.activeGameObject.transform, false);
+    }
+#endif
+
+    public int maximum;
+    public int minimum;
+    public int current;
+    public Image mask;
+    public Image fill;
+    public Color color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float currentOffset = current - minimum;
+        float maximumOffset = maximum - minimum;
+        float fillamount = currentOffset / maximumOffset;
+        mask.fillAmount = fillamount;
+
+        fill.color = color;
+    }
+    
+}
